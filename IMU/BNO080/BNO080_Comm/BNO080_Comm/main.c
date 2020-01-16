43683bdb281a31e6d13799d1004f9697079596e8
RBOLDI <Rowan.Bolding@hva.nl>
2020-01-16 17:51:20 +0100

    spi.c en spi.h opgesteld

---
+++ IMU/BNO080/BNO080_Comm/BNO080_Comm/main.c
@@ -0,0 +1,19 @@
+/*
+ * BNO080_Comm.c
+ *
+ * Created: 1/16/2020 5:44:14 PM
+ * Author : Rowan
+ */ 
+
+#include <avr/io.h>
+#include "spi.h"
+
+int main(void)
+{
+	spi_init();
+    while (1) 
+    {
+		
+    }
+}
+
---
+++ IMU/BNO080/BNO080_Comm/BNO080_Comm/spi.h
@@ -0,0 +1,28 @@
+/*
+ * spi.h
+ *
+ * Created: 1/16/2020 5:46:23 PM
+ *  Author: Rowan
+ */ 
+
+
+#ifndef SPI_H_
+#define SPI_H_
+
+#define SPI_SS_bm      0x10
+#define SPI_MOSI_bm    0x20
+#define SPI_MISO_bm    0x40
+#define SPI_SCK_bm     0x80
+
+#define FOO            0x00
+
+void    spi_init(void);
+uint8_t spi_transfer(uint8_t data);
+void    spi_write(uint8_t data);
+uint8_t spi_read(void);
+
+
+
+
+
+#endif /* SPI_H_ */
